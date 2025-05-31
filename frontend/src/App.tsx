a890326f6c34f71de3e1ee585f1c72af49e20e73
raydebug <raydebug@gmail.com>
2025-05-31 21:28:20 +1000

    Fix: Add ThemeProvider to resolve styled-components theme errors - Wrap App with ThemeProvider and import theme - Fixes 'Cannot read properties of undefined (reading background)' errors - Enables Avatar and other theme-dependent components to work in tests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import LobbyPage from './pages/LobbyPage';
 import GamePage from './pages/GamePage';
 import { JoinGamePage } from './pages/JoinGamePage';
@@ -7,22 +8,25 @@ import { JoinPage } from './pages/JoinPage';
 import { AuthDemo } from './pages/AuthDemo';
 import { Navigation } from './components/Navigation';
 import GlobalStyles from './styles/GlobalStyles';
+import { theme } from './styles/theme';
 
 function App() {
   return (
-    <Router>
-      <GlobalStyles />
-      <div data-testid="game-container">
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<LobbyPage />} />
-          <Route path="/auth-demo" element={<AuthDemo />} />
-          <Route path="/join" element={<JoinPage />} />
-          <Route path="/join-table" element={<JoinGamePage />} />
-          <Route path="/game/:gameId" element={<GamePage />} />
-        </Routes>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <GlobalStyles />
+        <div data-testid="game-container">
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<LobbyPage />} />
+            <Route path="/auth-demo" element={<AuthDemo />} />
+            <Route path="/join" element={<JoinPage />} />
+            <Route path="/join-table" element={<JoinGamePage />} />
+            <Route path="/game/:gameId" element={<GamePage />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
